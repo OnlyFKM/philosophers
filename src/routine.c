6bfadb20c8538198d6e178b48343a1ec79426956
FKM11 <fjcastillomartin@gmail.com>
2023-12-19 12:06:50 +0100

    commit

--- src/routine.c
+++ src/routine.c
@@ -6,34 +6,21 @@
 /*   By: frcastil <frcastil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 19:40:22 by frcastil          #+#    #+#             */
-/*   Updated: 2023/12/19 11:01:22 by frcastil         ###   ########.fr       */
+/*   Updated: 2023/12/19 12:05:50 by frcastil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
 
-void	ft_check_all_eat(t_program *program)
+void	ft_check_all_eat(t_philo *philo)
 {
-	int	i;
+	t_program	*program;
 
-	while (1)
-	{
-		i = 0;
-		while (i < program->number_philos)
-		{
-			if (program->philo[i].times_philo_has_eaten < program->number_meals)
-				break ;
-			i++;
-		}
-		if (i == program->number_philos)
-		{
-			program->all_philos_have_eaten++;
-			if (program->all_philos_have_eaten == program->number_meals)
-				break ;
-		}
-		if (program->philo_died)
-			break ;
-	}
+	program = philo->program;
+	if (philo->times_philo_has_eaten == program->number_meals)
+		program->all_philos_have_eaten++;
+	if (program->all_philos_have_eaten == program->number_philos)
+		exit (EXIT_FAILURE);
 }
 
 void	ft_eating(t_philo *philo)
@@ -50,6 +37,7 @@ void	ft_eating(t_philo *philo)
 	philo->time_last_meal = ft_get_time();
 	pthread_mutex_unlock(&(program->meal_mutex));
 	philo->times_philo_has_eaten++;
+	ft_check_all_eat(philo);
 	ft_usleep(program->time_eat);
 	pthread_mutex_unlock(&(program->forks[philo->left_fork_id]));
 	pthread_mutex_unlock(&(program->forks[philo->right_fork_id]));
@@ -68,15 +56,15 @@ void	*ft_routine(void *arg)
 	{
 		ft_eating(philo);
 		ft_check_if_dead(program);
-		if (program->all_philos_have_eaten == 1)
-			return (NULL);
+		/* if (program->all_philos_have_eaten == 1)
+			return (NULL); */
 		ft_printf_msg(program, philo->philo_id, "is sleeping");
 		ft_usleep(program->time_sleep);
-		if (program->all_philos_have_eaten == 1)
-			return (NULL);
+		/* if (program->all_philos_have_eaten == 1)
+			return (NULL); */
 		ft_printf_msg(program, philo->philo_id, "is thinking");
-		if (program->all_philos_have_eaten == 1)
-			return (NULL);
+		/* if (program->all_philos_have_eaten == 1)
+			return (NULL); */
 	}
 	return (NULL);
 }
